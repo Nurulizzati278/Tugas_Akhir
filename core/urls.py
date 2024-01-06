db0493e59decb493ad9aa8527ea47b222b914847
Nurul izzati <nur.izzati@mhs.unsyiah.ac.id>
2024-01-06 22:50:47 +0700

    Perubahan yuyun

--- core/urls.py
+++ core/urls.py
@@ -16,17 +16,33 @@ urlpatterns = [
     
     
     # jangan ganggu yang baru
-    path("panduan_masyarakat/", views.panduan_masyarakat, name="panduan_masyarakat"),
-    path("loginp/", views.loginp, name="loginp"),
     # batassuci
     path("home/", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("profile/", views.profile, name="profile"),
-    path("tambah_data/", views.tambah_data, name="tambah_data"),
-    path("penilaian/", views.penilaian, name="penilaian"),
+    path("PengajuanSekolah/", views.PengajuanSekolah, name="PengajuanSekolah"),
     path("koordinat/", views.koordinat, name="koordinat"),
     path("edit_data/", views.edit_data, name="edit_data"),
-    path("edit_penilaian/", views.edit_penilaian, name="edit_penilaian"),
     path("edit_koordinat/", views.edit_koordinat, name="edit_koordinat"),
+    path("pengajuan_akreditasi/", views.pengajuan_akreditai, name="pengajuan_akreditasi"),
     path("panduan/", views.panduan, name="panduan"),
+    path("PengajuanSekolah/",views.PengajuanSekolah, name="PengajuanSekolah")
+    
+    #path("edit_penilaian/", views.edit_penilaian, name="edit_penilaian"),
+    #path("penilaian/", views.penilaian, name="penilaian"),
+    #path("nilai_akhir/", views.nilai_akhir, name="nilai_akhir"),
+    
+    #path("standar_kompetensi/", views.standar_kompetensi, name="standar_kompetensi"),
+    #path("standar_isi/", views.standar_isi, name="standar_isi"),
+    #path("standar_prose/", views.standar_proses, name="standar_proses"),
+    #path("standar_penilaian_pendidikan/", views.standar_penilaian_pendidikan, name="standar_penilaian_pendidikan"),
+    #path("standar_tenaga_kependidikan/", views.standar_tenaga_kependidikan, name="standar_tenaga_kependidikan"),
+    #path("standar_sarana_prasarana/", views.standar_sarana_prasarana, name="standar_sarana_prasarana"),
+    #path("standar_pengelolaan/", views.standar_pengelolaan, name="standar_pengelolaan"),
+    #path("standar_pembiayaan/", views.standar_pembiayaan, name="standar_pembiayaan"),
+    
+    #path("komponen_mutu_lulusan/", views.komponen_mutu_lulusan, name="komponen_mutu_lulusan"),
+    #path("komponen_proses_pembelajaran/", views.komponen_proses_pembelajaran, name="komponen_proses_pembelajaran"),
+    #path("komponen_mutu_guru/", views.komponen_mutu_guru, name="komponen_mutu_guru"),
+    #path("komponen_manajemen_sekolah/", views.komponen_manajemen_sekolah, name="komponen_manajemen_sekolah"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from . import models
 
 # masyarakat
 def landing_page(request):
@@ -29,7 +30,7 @@ def kontak(request):
 def loginadmin(request):
     return render(request, "core/login.html")
 
-# fitur login nya ya sayang
+# fitur login
 from django.contrib.auth import authenticate, login
 
 def user_login(request):
@@ -44,7 +45,7 @@ def user_login(request):
             # User terotentikasi, lakukan login
             login(request, user)
             messages.success(request, 'Anda berhasil login')
-            return redirect('dashboard')  # Redirect setelah login berhasil
+            return redirect('home')  # Redirect setelah login berhasil
         else:
             # Autentikasi pengguna gagal
             messages.error(request, 'Kamu salah Password dan Username.')
@@ -59,15 +60,72 @@ def logoutadmin(request):
     request.session.flush()
     return redirect('loginadmin')
 
+def create_PengajuanSekolah(request):
+    if request.method == 'POST':
+        Pengajuan_sekolah = PengajuanSekolah(request.POST)
 
+        if Pengajuan_sekolah.is_valid():
+            # Membuat objek Sekolah dan mengisi kolom-kolomnya
+            sekolah = Sekolah.objects.create(
+                NamaSekolah=request.POST['NamaSekolah'],
+                NamaKepalaSekolah=request.POST['NamaKepalaSekolah'],
+                NamaPengawasSekolah=request.POST['NamaPengawasSekolah'],
+                AlamatSekolah=request.POST['AlamatSekolah'],
+                JumlahGuruPNS=request.POST['JumlahGuruPNS'],
+                JumlahGuruHonorer=request.POST['JumlahGuruHonorer'],
+                JumlahStaf=request.POST['JumlahStaf'],
+                JumlahSiswaX=request.POST['JumlahSiswaX'],
+                JumlahSiswaXI=request.POST['JumlahSiswaXI'],
+                JumlahSiswaXII=request.POST['JumlahSiswaXII'],
+                JumlahRuangKelas=request.POST['JumlahRuangKelas'],
+                StatusAkreditasi=request.POST['StatusAkreditasi']
+            )
+            
+            # Mengisi kolom-kolom pada objek Sekolah
+            PengajuanSekolah = PengajuanSekolah.save(commit=False)
+            PengajuanSekolah.sekolahId = sekolah
+            pengajuan.NamaSekolah = request.POST['NamaSekolah']
+            PengajuanSekolah.NamaKepalaSekolah = request.POST['NamaKepalaSekolah']
+            PengajuanSekolah.NamaPengawasSekolah = request.POST['NamaPengawasSekolah']
+            PengajuanSekolah.AlamatSekolah = request.POST['AlamatSekolah']
+            PengajuanSekolah.JumlahGuruPNS = request.POST['JumlahGuruPNS']
+            PengajuanSekolah.JumlahGuruHonorer = request.POST['JumlahGuruHonorer']
+            PengajuanSekolah.JumlahStaf = request.POST['JumlahStaf']
+            PengajuanSekolah.JumlahSiswaX = request.POST['JumlahSiswaX']
+            PengajuanSekolah.JumlahSiswaXI = request.POST['JumlahSiswaXI']
+            PengajuanSekolah.JumlahSiswaXII = request.POST['JumlahSiswaXII']
+            PengajuanSekolah.JumlahRuangKelas = request.POST['JumlahRuangKelas']
+            PengajuanSekolah.StatusAkreditasi = request.POST['StatusAkreditasi']
+            #if 'GambarSekolah' in request.FILES:
+             #   PengajuanSekolah.GambarSekolah = request.FILES['GambarSekolah']
 
+            # Simpan objek PengajuanSekolah
+            PengajuanSekolah.save()
+            
+            # Dapatkan objek Status dengan nilai 'DIAJUKAN'
+            status = Status.objects.create(tipestatus='DIAJUKAN')
 
-def panduan_masyarakat(request):
-    return render(request, "masyarakat/panduan_masyarakat.html")
+            # Menghubungkan objek PengajuanSekolah dengan objek status
+            PengajuanSekolah.statusPengajuanSekolah = status
+            PengajuanSekolah.save()
+            
+            messages.success(request, 'Berhasil diajukan')
+            return redirect('dashboard')  # Ganti 'peta' dengan URL halaman peta
+
+        else:
+            errors = PengajuanSekolah_form.errors
+            print(errors)
+            messages.error(request, 'Terjadi kesalahan dalam menambahkan data.')
+
+    else:
+        PengajuanSekolah_form = PengajuanSekolahForm()
+
+    return render(request, 'admin_sekolah/dashboard.html', {'Pengajuan_sekolah': Pengajuan_sekolah})
+
 
 # admin sekolah
-def loginp(request):
-    return render(request, "admin_sekolah/login.html")
+#def loginp(request):
+#    return render(request, "admin_sekolah/login.html")
 
 def home(request):
     return render(request, "admin_sekolah/home.html")
@@ -79,11 +137,11 @@ def dashboard(request):
 def profile(request):
     return render(request, "admin_sekolah/profile.html")
 
-def tambah_data(request):
-    return render(request, "admin_sekolah/tambah_data.html")
+def PengajuanSekolah(request):
+    return render(request, "admin_sekolah/PengajuanSekolah.html")
 
-def penilaian(request):
-    return render(request, "admin_sekolah/penilaian.html")
+#def penilaian(request):
+#    return render(request, "admin_sekolah/penilaian.html")
 
 def koordinat(request):
     return render(request, "admin_sekolah/koordinat.html")
@@ -91,11 +149,58 @@ def koordinat(request):
 def edit_data(request):
     return render(request, "admin_sekolah/edit_data.html")
 
-def edit_penilaian(request):
-    return render(request, "admin_sekolah/edit_penilaian.html")
+#def edit_penilaian(request):
+#    return render(request, "admin_sekolah/edit_penilaian.html")
 
 def edit_koordinat(request):
     return render(request, "admin_sekolah/edit_koordinat.html")
 
+def pengajuan_akreditai(request):
+    return render(request, "admin_sekolah/pengajuan_akreditasi.html")
+
 def panduan(request):
     return render(request, "admin_sekolah/panduan.html")
+
+def nilai_akhir(request):
+    return render(request, "admin_sekolah/nilai_akhir.html")
+
+#pengajuan akreditasi
+
+#8 SNP
+#def standar_kompetensi(request):
+ #   return render(request, "snp/standar_kompetensi.html")
+
+#def standar_isi(request):
+#    return render(request, "snp/standar_isi.html")
+
+#def standar_proses(request):
+#   return render(request, "snp/standar_proses.html")
+
+#def standar_penilaian_pendidikan(request):
+#    return render(request, "snp/standar_penilaian_pendidikan.html")
+
+#def standar_tenaga_kependidikan(request):
+#    return render(request, "snp/standar_tenaga_kependidikan.html")
+
+#def standar_sarana_prasarana(request):
+#    return render(request, "snp/standar_sarana_prasarana.html")
+
+#def standar_pengelolaan(request):
+#    return render(request, "snp/standar_pengelolaan.html")
+
+#def standar_pembiayaan(request):
+#    return render(request, "snp/standar_pembiayaan.html")
+
+#IASP
+def komponen_mutu_lulusan(request):
+    return render(request, "IASP/komponen_mutu_lulusan.html")
+
+def komponen_proses_pembelajaran(request):
+    return render(request, "IASP/komponen_proses_pembelajaran.html")
+
+def komponen_mutu_guru(request):
+    return render(request, "IASP/komponen_mutu_guru.html")
+
+def komponen_manajemen_sekolah(request):
+    return render(request, "IASP/komponen_manajemen_sekolah.html")
+
