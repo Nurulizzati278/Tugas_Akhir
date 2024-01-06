efe343ce32f3b0ad9e0c5078eb5d700d1724aaba
YoviePramudya <yoviepramudyasabri@gmail.com>
2024-01-07 03:03:26 +0700

    upp branch baru

--- akreditasi_sekolah/settings.py
+++ akreditasi_sekolah/settings.py
@@ -82,20 +82,20 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = "akreditasi_sekolah.wsgi.application"
 
-GEOS_LIBRARY_PATH="C:/OSGeo4W/bin/geos_c.dll"
+# GEOS_LIBRARY_PATH="C:/OSGeo4W/bin/geos_c.dll"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "nurulizzt",
-        "USER": "postgres",
-        "PASSWORD": "yovie123",
-        "HOST": "localhost",  # Ganti sesuai dengan host PostgreSQL Anda
-        "PORT": "5432",  # Port default PostgreSQL adalah 5432
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.contrib.gis.db.backends.postgis",
+#         "NAME": "nurulizzt",
+#         "USER": "postgres",
+#         "PASSWORD": "yovie123",
+#         "HOST": "localhost",  # Ganti sesuai dengan host PostgreSQL Anda
+#         "PORT": "5432",  # Port default PostgreSQL adalah 5432
+#     }
+# }
 
 GDAL_LIBRARY_PATH = "C:/OSGeo4W/bin/gdal308.dll"
 
